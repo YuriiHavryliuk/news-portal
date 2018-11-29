f65900a5a40d90eeeb098c6a1ca7fe850387e67a
YuriiHavryliuk <gawrelyuk@gmail.com>
2018-11-29 02:22:01 +0200

    Added shape text and propTypes

--- src/components/ArticleList/components/Article.js
+++ src/components/ArticleList/components/Article.js
@@ -1,20 +1,31 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+import shave from 'shave'
 
 import style from './style.css'
 import Comments from '../../Comments'
 
-export default class Article extends Component {
+export default class Article extends PureComponent {
   constructor(props) {
     super(props)
 
     this.state = {
-      visible: false,
+      isOpen: false,
     }
+  }
+
+  componentDidMount() {
+    shave(this.node, this.computedHeight())
+    window.addEventListener('resize', this.handleResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize)
   }
 
   render() {
     const { article } = this.props
-    const { visible } = this.state
+    const { isOpen } = this.state
     return (
       <div>
         <div className="article__header">
@@ -24,23 +35,68 @@ export default class Article extends Component {
               onClick={this.showsText}
               className="btn-primary article__btn"
             >
-              {visible ? 'hide' : 'show'} article
+              {isOpen ? 'hide' : 'show'} article
             </button>
           </div>
         </div>
-        {visible && (
-          <div>
+
+        <div>
+          <p
+            className="article"
+            ref={node => {
+              this.node = node
+            }}
+          >
             {article.text}
-            <div>
-              <b>{article.date}</b>
-            </div>
-            <Comments comments={article.comments} />
+          </p>
+          <div>
+            <b>{article.date}</b>
           </div>
-        )}
+          {isOpen && <Comments comments={article.comments} />}
+        </div>
       </div>
     )
   }
-  showsText = e => {
-    this.setState(prevState => ({ visible: !prevState.visible }))
+
+  computedHeight = () => {
+    const line = 2
+    const style = getComputedStyle(this.node)
+    const fontSize = parseInt(style.getPropertyValue('font-size'), 10)
+    let lineh = style.getPropertyValue('line-height')
+
+    if (
+      (!lineh && lineh !== 0) ||
+      String('normal|initial|inherit').indexOf(lineh) > -1
+    ) {
+      lineh = fontSize
+    }
+    return parseFloat(lineh) * line
   }
+
+  toggleText = () => {
+    const { isOpen } = this.state
+    const height = isOpen ? Infinity : this.computedHeight()
+
+    shave(this.node, height)
+  }
+
+  showsText = e => {
+    this.setState(
+      prevState => ({ isOpen: !prevState.isOpen }),
+      () => this.toggleText()
+    )
+  }
+
+  handleResize = () => {
+    this.toggleText()
+  }
+}
+
+Article.propTypes = {
+  article: PropTypes.shape({
+    text: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    comments: PropTypes.array,
+    date: PropTypes.string.isRequired,
+  }),
 }
